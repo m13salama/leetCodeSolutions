6b3cd0c9de549cc8ab51e1d08118c33850a76e5f
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2022-05-25 12:24:51 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (79.16%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        cin.tie(0);
-        cout.tie(0);
-        cin.sync_with_stdio(0);
-        cout.sync_with_stdio(0);        
-        return (__builtin_popcount(n));
+        int count=0;
+        while(n!=0){
+        if(n&1) 
+            count++;
+          n= n>>1;
+        }
+        return count;
     }
 };
