0f42c90c75179629b92985c12a1b0e3201b087f0
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2022-05-26 04:52:21 +0200

    Time: 20 ms (81.56%), Space: 17 MB (22.60%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        cin.tie(0);
-        cout.tie(0);
+        cin.tie(NULL);
+        cout.tie(NULL);
         cin.sync_with_stdio(0);
         cout.sync_with_stdio(0);
-        int res = 0;
-        for(int i=0; i<nums.size(); i++){
+        int res = nums[0];
+        for(int i=1; i<nums.size(); i++){
             res = res ^ nums[i];
         }       
         return (res);
