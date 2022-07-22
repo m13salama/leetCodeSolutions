deb9feeeb6d63cdb22772d0252315b87c147811e
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2022-07-22 05:35:21 +0200

    Time: 0 ms (100.00%), Space: 6.4 MB (19.19%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        map<int,int> memo;
+        return fib_memo(n, memo);
+
+    }
+    int fib_memo(int n, map<int,int>& memo) {
+        if(memo[n] != 0){
+            return memo[n];
+        } 
+        if(n == 1 || n ==2) return 1;
+        else if(n==0) return 0;
+        else{
+            int result = fib_memo(n-1,memo) + fib_memo(n-2, memo) + fib_memo(n-3, memo);
+            memo[n] = result;
+            return result;
+        }
+    }
+};
