2fd0e2bfc8c145c6844190430b055daaafe3386e
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2022-07-26 12:10:19 +0200

    Time: 0 ms (100.00%), Space: 6.2 MB (82.47%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -11,11 +11,12 @@ public:
         int p1=0, p2=0;
         string s= "";
         while(p1<word1.size() && p2<word2.size()){
-            s = s+word1[p1]+word2[p2];
+            s += word1[p1];
+            s += word2[p2];
             p1++; p2++;
         }
-        while(p1<word1.size()) s = s+word1[p1++];
-        while(p2<word2.size()) s = s+word2[p2++];
+        while(p1<word1.size()) s += word1[p1++];
+        while(p2<word2.size()) s += word2[p2++];
         return s;
 
     }
