f1d8798571eedbd2362dfc78b85d8fe60f77630e
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2022-08-02 12:48:15 +0200

    Time: 11 ms (47.18%), Space: 8.4 MB (42.11%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -8,12 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-const static auto fast = []{
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
+
 
 class Solution {
 public:
