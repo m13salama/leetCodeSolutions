b2c841f15ffffacb966a40b356d13a085ef80b08
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2022-08-16 17:25:22 +0200

    Time: 51 ms (69.16%), Space: 27.6 MB (12.32%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,6 +1,9 @@
+#define init  cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0)
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        init;
         int l=0, r=nums.size()-1;
         int mid =0;
         while(l<=r){
