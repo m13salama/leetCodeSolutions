54b2b7a26eb9e19cd8f951ca71fded1a12691171
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2022-08-28 15:59:35 +0200

    Time: 36 ms (49.60%), Space: 14.4 MB (34.82%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,33 @@
+#define init cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0)
+
+class Solution {
+public:
+    map<int, int> visited;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        init;
+        int res;
+        vector<vector<int>> pre(numCourses);
+        for (int i=0; i<prerequisites.size(); i++){
+            pre[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        for (int i=0; i<numCourses; i++){
+            if(!visited[i]) res = dfs(pre, i);
+            if(res == 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    int dfs(vector<vector<int>>& pre, int course){
+        if(pre[course].size() == 0) return 1;
+        if(visited[course] != 0) return 0;
+        visited[course]++;
+        for(int i=0; i<pre[course].size(); i++){
+            if (!dfs(pre, pre[course][i])) return 0;
+        }
+        visited[course]--;
+        pre[course].clear();
+        return 1;
+    }
+};
