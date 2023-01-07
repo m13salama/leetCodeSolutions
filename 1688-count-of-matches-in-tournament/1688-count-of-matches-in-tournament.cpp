62634099ea370745a363de89b9b1a3e1bf007334
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2023-01-07 09:22:43 +0200

    Time: 0 ms (100.00%), Space: 5.8 MB (83.01%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
+        //every match we eliminate 1 team
+        //we need (n-1) matches to eliminate (n-1) teams
+        //the remaining one is the champion
         return n-1;
     }
 };
