d8b98f3768c2faa848f4790e21391e4c337b9389
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2023-01-18 17:03:50 +0200

    Time: 68 ms (72.93%), Space: 48.4 MB (79.56%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+#define init cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0)
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int max_ava = nums[0]+1;
+        
+        if(n == 1) return true;
+        
+        for(int i=0; i<max_ava; i++){
+            max_ava = max(max_ava, i+nums[i]+1);
+            if(max_ava >= n) return true;
+        }
+        return false;
+    
+    }
+};
