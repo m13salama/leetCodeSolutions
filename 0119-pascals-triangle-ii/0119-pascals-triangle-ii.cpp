0dc092a897e0962ab86b49c6a070faa61d8b25cb
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2023-01-26 14:38:59 +0200

    Time: 0 ms (100.00%), Space: 6.7 MB (8.76%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,21 @@
+#define init cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0)
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        init;
+        
+        vector<vector<int>> pascal(rowIndex+1);
+        pascal[0].push_back(1);
+        
+        for(int i=1; i<rowIndex+1; i++){
+            pascal[i].push_back(1);
+            for(int j=1; j<i; j++){
+                pascal[i].push_back(pascal[i-1][j]+pascal[i-1][j-1]);
+            }
+            pascal[i].push_back(1);
+        }
+        return pascal[rowIndex];
+        
+    }
+};
