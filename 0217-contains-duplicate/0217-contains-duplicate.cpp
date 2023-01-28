405c7d20f6e7eebe9c96a55d73edb2077f7c3cc1
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2023-01-28 10:07:42 +0200

    Time: 234 ms (18.64%), Space: 70.8 MB (6.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,8 @@
+#define init cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0)
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        init;
         map<int, int> momo;
         int n=nums.size();
         
