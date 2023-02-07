2f84a1a561baf533c633e75ad537824d75006de5
Mohamed salama <68412123+m13salama@users.noreply.github.com>
2023-02-07 22:36:19 +0200

    Time: 4 ms (77.56%), Space: 7.5 MB (51.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,6 +1,8 @@
+#define init cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0)
 class Solution {
 public:
     bool isPalindrome(string s) {
+        init;
         if(s.empty())return false;
         int i=0;
         int j=s.length()-1;
